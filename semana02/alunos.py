8e3d46fbe81c29cfbbbd8edc2354df1c98a5b4a1
MariaBet <mariabetania543@gmail.com>
2020-06-16 15:16:14 -0300

    Aula streamlit

---
+++ semana02/alunos.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title("AcerelaDev Data Science")
+    st.image('logo.png')
+    file = st.file_uploader('Upload your file', type = 'csv')
+    if file is not None:
+        slider = st.slider('Valores', 1,100)
+        df = pd.read_csv(file)
+        st.dataframe(df.head(slider))
+        st.markdown('Markdown')
+        st.table(df.head(slider))
+        st.write(df.columns)
+        
+
+if __name__ == '__main__':
+    main()
